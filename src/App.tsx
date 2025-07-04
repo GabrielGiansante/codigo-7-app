af84b0bb4da3c634a344567540eb70addba1e9cd
Gabriel Giansante <gabrielfsgiansante@hotmail.com>
2025-07-04 15:20:43 -0300

    Muda para flex-start e ajusta margin-top para corrigir rolagem da reserva

--- src/App.tsx
+++ src/App.tsx
@@ -183,7 +183,7 @@ function App() {
   return (
     <div style={{
       minHeight: '100vh', display: 'flex', flexDirection: 'column',
-      alignItems: 'center', justifyContent: 'center',
+      alignItems: 'center', justifyContent: 'flex-start',
       paddingTop: '40px', paddingBottom: '40px', boxSizing: 'border-box'
     }}>
       {/* ... Telas de boasVindas, cadastroTelefone, verificacaoCodigo ... */}
